46a3bbf958eb79294d12a9b5ecdf76b410154182
EFinish <EFinish@gmail.com>
2023-04-19 10:42:45 +0200

    add static dep

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,12 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
 
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
+  kit: {
+    adapter: adapter(),
+    prerender: {
+      default: true,
+    },
+  },
   preprocess: vitePreprocess(),
 }
